cc388d6435afaeeeb38c693d5c764b3149cd5845
gattoguilherme <guilhermegattoyo@hotmail.com>
2019-03-01 22:20:23 -0300

    commit

---
+++ PDM-master/app/src/main/java/com/example/matheusrubortone/exercicio_aula_01/Internacionalizacao.java
@@ -0,0 +1,13 @@
+package com.example.matheusrubortone.exercicio_aula_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Internacionalizacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internacionalizacao);
+    }
+}
